5bb3608535ba313a2bcb0bd6e2eebc5916e1719f
Devin Cowan <dcowan@cuahsi.org>
2023-03-22 09:08:22 -0400

    update init certbot script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# from https://github.com/wmnnd/nginx-certbot
+# modified from https://github.com/wmnnd/nginx-certbot
 
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
@@ -9,7 +9,7 @@ fi
 # TODO: sed domains instead of hard-coding
 domains=(usagemetrics.hydroshare.org kibana.usagemetrics.hydroshare.org es.usagemetrics.hydroshare.org caddy.usagemetrics.hydroshare.org logstash.usagemetrics.hydroshare.org)
 rsa_key_size=4096
-data_path="./"
+data_path="./certbot"
 email="dev@cuahsi.org" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
@@ -32,7 +32,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+docker-compose -f docker-compose.yml -f docker-compose-prod.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -45,7 +45,7 @@ docker-compose up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
+docker-compose -f docker-compose.yml -f docker-compose-prod.yml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -68,7 +68,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+docker-compose -f docker-compose.yml -f docker-compose-prod.yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
