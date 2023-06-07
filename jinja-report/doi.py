273a1baa7ff200c8634666d07b5c8f64e2884668
Devin Cowan <dcowan@cuahsi.org>
2023-06-07 14:15:12 -0400

    more experimental plots

--- jinja-report/doi.py
+++ jinja-report/doi.py
@@ -6,6 +6,7 @@ import pickle
 import requests
 #import argparse
 import pandas as pd
+import numpy
 from lxml import etree
 import hs_restclient as hsapi
 from datetime import datetime
@@ -13,6 +14,9 @@ from datetime import datetime
 #import matplotlib.dates as mdates
 from urllib3.exceptions import InsecureRequestWarning
 from pandas.plotting import register_matplotlib_converters
+from organizations import load_data as org_load_data
+from matplotlib.pyplot import cm
+import users
 
 
 import plot
@@ -92,6 +96,242 @@ def published_resources(input_directory='.',
     return plot.PlotObject(x, y,
                            label=label,
                            color=color)
+
+def publications_by_usr_type(input_directory='.',
+                        start_time=datetime(2000, 1, 1),
+                        end_time=datetime(2030, 1, 1),
+                        aggregation='1M',
+                        usertypes=[],
+                        label='Monthly published resources',
+                        color='b',
+                        **kwargs):
+    print(f'--> calculating published resources per {aggregation}')
+    df = load_data(input_directory)
+    df = utilities.subset_by_date(df,
+                                  start_time,
+                                  end_time,
+                                  date_column='Date Published')
+    
+    # link owner_id to a user and get their institution
+    org_df = org_load_data(input_directory)
+    org_df['usr_id'] = pd.to_numeric(org_df['usr_id'],errors='coerce')
+    df = df.merge(org_df, right_on='usr_id', left_on='owner_id', how='left')
+    df.set_index('Date Published', inplace=True)
+    # now we have usr_organization and usr_type
+
+    # define HS user types
+    ut_vocab = ['Post-Doctoral Fellow',
+                'Commercial/Professional',
+                'University Faculty',
+                'Government Official',
+                'University Graduate Student',
+                'Professional',
+                'University Professional or Research Staff',
+                'Local Government',
+                'University Undergraduate Student',
+                'School Student Kindergarten to 12th Grade',
+                'School Teacher Kindergarten to 12th Grade',
+                'Other']
+
+    # clean the data
+    df.loc[~df.usr_type.isin(ut_vocab), 'usr_type'] = 'Other'
+    
+    # loop through each of the user types
+    plots = []
+
+    # plot only the provided user types
+    if len(usertypes) == 0:
+        # select all unique user types
+        usertypes = df.usr_type.unique()
+
+    colors = iter(cm.jet(numpy.linspace(0, 1, len(usertypes))))
+    for utype in usertypes:
+
+        # group by user type
+        du = df.loc[df.usr_type == utype]
+
+        # remove null values
+        # du = du.dropna()
+
+        # group by date frequency
+        ds = du.groupby(pd.Grouper(freq=aggregation)).count().usr_type.cumsum()
+        x = ds.index
+        y = ds.values
+        c = next(colors)
+
+        # create plot object
+        plots.append(plot.PlotObject(x, y, label=utype, color=c, linestyle='-'))
+
+    return plots
+
+def publications_by_org_type(input_directory='.',
+                        start_time=datetime(2000, 1, 1),
+                        end_time=datetime(2030, 1, 1),
+                        aggregation='1M',
+                        orgtypes=[],
+                        label='Monthly published resources',
+                        **kwargs):
+    print(f'--> calculating published resources per {aggregation}')
+    df = load_data(input_directory)
+    df = utilities.subset_by_date(df,
+                                  start_time,
+                                  end_time,
+                                  date_column='Date Published')
+    
+    # link owner_id to a user and get their institution
+    org_df = org_load_data(input_directory)
+    org_df['usr_id'] = pd.to_numeric(org_df['usr_id'],errors='coerce')
+    df = df.merge(org_df, right_on='usr_id', left_on='owner_id', how='left')
+    df.set_index('Date Published', inplace=True)
+    # now we have usr_organization and usr_type
+
+    # clean the data
+    df = df.loc[df.usr_organization != 'None']
+    
+    # loop through each of the user types
+    plots = []
+
+    # plot only the provided org types
+    if len(orgtypes) == 0:
+        # select all unique user types
+        orgtypes = df.usr_organization.unique()
+
+    colors = iter(cm.jet(numpy.linspace(0, 1, len(orgtypes))))
+    for otype in orgtypes:
+
+        # group by org type
+        du = df.loc[df.usr_organization == otype]
+
+        # remove null values
+        # du = du.dropna()
+
+        # group by date frequency
+        ds = du.groupby(pd.Grouper(freq=aggregation)).count().usr_organization.cumsum()
+        x = ds.index
+        y = ds.values
+        c = next(colors)
+
+        # create plot object
+        plots.append(plot.PlotObject(x, y, label=otype, color=c, linestyle='-'))
+
+    return plots
+
+def orgpie(input_directory='.',
+        start_time=datetime(2000, 1, 1),
+        end_time=datetime(2030, 1, 1),
+        exclude=['None'],
+        publication_threshold=1,
+        **kwargs):
+
+    print('--> building doi org types pie-chart')
+
+    df = _join_orgs_with_dois(input_directory,
+        start_time, end_time,
+        exclude,
+        publication_threshold,
+        **kwargs)
+
+    return plot.PlotObject(None,
+                           df.percent,
+                           dataframe=df,
+                           label='percent')
+
+def orgtable(input_directory='.',
+        start_time=datetime(2000, 1, 1),
+        end_time=datetime(2030, 1, 1),
+        exclude=['None'],
+        publication_threshold=1,
+        **kwargs):
+
+    print('--> building doi org types pie-chart')
+
+    df = _join_orgs_with_dois(input_directory,
+        start_time, end_time,
+        exclude,
+        publication_threshold,
+        **kwargs)
+    
+    df.reset_index(inplace=True)
+    df = df.rename(columns = {'type':'Organizations', 'score':'Score', 'percent':'Percent'})
+
+    return plot.PlotObject(None, None,
+                           label=kwargs.get('label', 'Percent'),
+                           dataframe=df)
+
+def _join_orgs_with_dois(input_directory='.',
+        start_time=datetime(2000, 1, 1),
+        end_time=datetime(2030, 1, 1),
+        exclude=['None'],
+        publication_threshold=1,
+        **kwargs):
+    df = load_data(input_directory)
+    df = utilities.subset_by_date(df,
+                                  start_time,
+                                  end_time,
+                                  date_column='Date Published')
+    
+    # link owner_id to a user and get their institution
+    org_df = org_load_data(input_directory)
+    org_df['usr_id'] = pd.to_numeric(org_df['usr_id'],errors='coerce')
+    df = df.merge(org_df, right_on='usr_id', left_on='owner_id', how='left')
+    df.set_index('Date Published', inplace=True, drop=True)
+    # now we have usr_organization and usr_type
+
+    # clean the data
+    df = df.loc[df.usr_organization != 'None']
+    # df.dropna(inplace=True, subset=['usr_organization'])
+    df = df.filter(items=['usr_organization'])
+
+    # count number of users for each type
+    orgtypes = df.usr_organization.unique()
+    for o in orgtypes:
+        df[o] = numpy.where(df['usr_organization'] == o, 1, 0)
+    df['Other'] = numpy.where(~df['usr_organization'].isin(orgtypes), 1, 0)
+
+    # remove 'usr_organization' b/c it's no longer needed
+    # df = df.drop('usr_organization', axis=1)
+
+    # remove specified columns so they won't be plotted
+    for drp in exclude:
+        try:
+            print('--> not reporting %s: %s users'
+                  % (drp, df[drp].sum()))
+            df.drop(drp, inplace=True, axis=1)
+        except Exception as e:
+            print(f'Error dropping from doi orgs pie df: {e}')
+
+    # calculate total and percentages for each user type
+    ds = df.sum()
+
+    df = pd.DataFrame({'type': ds.index, 'score': ds.values})
+    df = df.set_index('type')
+    df['percent'] = round(df['score']/df['score'].sum()*100, 2)
+
+    print('--> total number of users reporting: %d' % df.score.sum())
+    others_pct = 0
+    others_score = 0
+    for u in orgtypes:
+        if u not in exclude:
+            pct = df.loc[u].percent
+            score = df.loc[u].score
+            if pct < publication_threshold:
+                others_pct += pct
+                others_score += score
+                print(f"Dropping Org: {u} because it has less than {publication_threshold}")
+                try:
+                    df = df.drop(u)
+                except ValueError:
+                    print(f"Error dropping {u}")
+                    df = df.rename({u: '%s (%2.2f%%)' % (u, pct)})
+            else:   
+                df = df.rename({u: '%s (%2.2f%%)' % (u, pct)})
+    print(f"Percent of values aggregated into 'other' = {others_pct}")
+    other_index = 'Other (%2.2f%%)' % (others_pct)
+    df.loc['Other', 'percent'] = others_pct
+    df.loc['Other', 'score'] = others_score
+    df = df.rename({'Other': other_index})
+    df.sort_values('score', inplace=True, ascending=False)
+    return df
 
 
 def citations(input_directory='.',
--- jinja-report/organizations.py
+++ jinja-report/organizations.py
@@ -52,29 +52,68 @@ def subset_by_date(dat, st, et):
 
 
 def total_table(input_directory='.',
-               start_time=datetime(2000, 1, 1),
-               end_time=datetime(2030, 1, 1),
-               num_records=50):
+                start_time=datetime(2000, 1, 1),
+                end_time=datetime(2030, 1, 1),
+                aggregation='Q',
+                **kwargs):
    
-   print('--> calculating total distinct organizations - table')
+    print('--> calculating total distinct organizations - table')
 
-   # load the data based on working directory and subset it if necessary
-   df = load_data(input_directory)
-   df = subset_by_date(df, start_time, end_time)
+    # load the data based on working directory and subset it if necessary
+    df = load_data(input_directory)
+    df = subset_by_date(df, start_time, end_time)
 
-   # drop duplicates (except the first occurrence)
-   df = df.drop_duplicates(subset='usr_organization', keep='first')
+    # drop duplicates (except the first occurrence)
+    # df = df.drop_duplicates(subset='usr_organization', keep='first')
 
-   # group and cumsum
-   df = df.sort_index()
-   ds = df.groupby(pandas.Grouper(freq=aggregation)) \
-          .usr_organization.nunique().cumsum()
+    df = df.filter(['usr_organization'], axis=1)
+
+    df.mask(df.eq('None')).dropna()
+
+    # create columns for each org
+    for uo in df.usr_organization.unique():
+        df[uo] = numpy.where(df.usr_organization == uo, 1, 0)
+    
+    # remove the usr_organization column since its been divided into
+    # individual columns
+    df.drop('usr_organization', axis=1, inplace=True)
+
+    df = df[df.columns[df.sum()> kwargs.get('users_threshold', 25)]]
+
+    # group and cumsum
+    # df = df.sort_index()
+    # ds = df.groupby(pandas.Grouper(freq=aggregation)) \
+    #         .usr_organization.nunique().cumsum()
+    df = df.groupby(pandas.Grouper(freq=aggregation)).sum()
+
+    # # drop unnecessary columns
+    # df = df.filter(['Date', 'begin_session', 'login', 'delete',
+    #                 'create', 'download', 'app_launch'], axis=1)
+
+    # # rename columns
+    # df = df.rename(columns={'begin_session': 'Begin\nSession',
+    #                         'login': 'Login',
+    #                         'delete': 'Delete\nResource',
+    #                         'create': 'Create\nResource',
+    #                         'download': 'Download\nResource',
+    #                         'app_launch': 'App\nLaunch'
+    #                         })
+
+    # modify the index to string type - for table printing
+    df.index = [item.strftime('%m-%d-%Y') for item in df.index]
+
+    # reverse the rows so that the table will be created in descending
+    # chronological order
+    df = df[::-1]
    
-   import pdb; pdb.set_trace()
+#    import pdb; pdb.set_trace()
 
-   # create plot object
-   x = ds.index
-   y = ds.values.tolist()
+    # create plot object
+    # x = ds.index
+    # y = ds.values.tolist()
+    return plot.PlotObject(None, None,
+                           label=kwargs.get('label', None),
+                           dataframe=df)
 
 
 def total(input_directory='.',
@@ -234,6 +273,7 @@ def org_type(input_directory='.',
              aggregation='1D',
              linestyle='-',
              orgtypes=[],
+             users_threshold=50,
              **kwargs):
 
     # load the data based on working directory
@@ -257,20 +297,26 @@ def org_type(input_directory='.',
 
     # # clean the data
     # df.loc[~df.usr_organization.isin(org_vocab), 'usr_organization'] = 'Other'
+    df = df.loc[df.usr_organization != 'None']
 
     # loop through each of the user types
     plots = []
 
-    # plot only the provided user types
+    # plot only the provided org types
     if len(orgtypes) == 0:
         # select all unique user types
         orgtypes = df.usr_organization.unique()
 
     colors = iter(cm.jet(numpy.linspace(0, 1, len(orgtypes))))
     for otype in orgtypes:
+        if otype == 'None':
+            continue
 
         # group by org type
         du = df.loc[df.usr_organization == otype]
+        if du.shape[0] < users_threshold:
+            next(colors)
+            continue
 
         # remove null values
         # du = du.dropna()
@@ -282,6 +328,6 @@ def org_type(input_directory='.',
         c = next(colors)
 
         # create plot object
-        plots.append(plot.PlotObject(x, y, label=otype, color=c, linestyle='-'))
+        plots.append(plot.PlotObject(x, y, label=otype, color=c, linestyle=linestyle))
 
     return plots
